303f5e40116cead63c2f1d6c217fecdad5b7a913
David Jachym <jachym19@gmail.com>
2023-09-05 20:16:16 -0500

    first commit

---
+++ py-assignment-1.py
@@ -0,0 +1,39 @@
+# Exercise 1 
+for num in range(1, 11):
+    print(num ** 3)
+
+# Exercise 2 
+for num in range(1, 101):
+    counter = 0
+    for num2 in range(1, 101):
+        if num % num2 == 0:
+            counter += 1
+    if counter == 2:
+        print(num)
+
+# Exercise 3
+go_again = True
+while go_again:
+    age = input("Enter your age: ")
+    
+    while age.isalpha():
+        print("Enter a numerical value...")
+        age = input("Enter your age: ")
+    
+    if age < '18':
+        print("Kids")
+    
+    elif age >= '18' and age <= '65':
+        print("Adults")
+    
+    else:
+        print("Seniors")
+    
+    run_again = input("Would you like to enter another age? (Y/N): ")
+    
+    while run_again.upper() not in ['Y', 'N']:
+        print("Enter Y to go again or N to terminate...")
+        run_again = input("Would you like to enter another age? (Y/N): ")
+    
+    if run_again.upper() == 'N':
+        go_again = False
